16d642933e863461e64b84560647d9ebfd3c63dc
eric-lim <eric.lim@xero.com>
2021-07-17 19:54:22 +1200

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,97 @@
+import { ReactComponent as YourSvg } from './builderscrack-logo.svg';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+      <div className="flex top-0 right-0 left-0 fixed bg-gray-100 space-x-4">
+        <YourSvg className="p-1"/>
+        <div>Button 1</div>
+        <div>Button 2</div>
+        <div>Button 3</div>
+      </div>
+      <div className="bg-indigo-300">
+        <div className="object-cover md:object-scale-down h-100 w-full">
+          <img src="https://builderscrack.co.nz/v4/bc/assets/backgrounds/homepage/pizza-sm.jpg.pagespeed.ce.Puw3czHKFU.jpg" />
+        </div>
+      </div>
+      <div className="text-center">
+        <button className="block py-2 px-4 bg-red-500 text-white font-semibold rounded-lg shadow-md hover:bg-red-700 focus:outline-none">
+          Hover me
+        </button>
+        <button className="block py-2 px-4 bg-red-500 text-white font-semibold rounded-lg shadow-md hover:bg-red-700 focus:outline-none">
+          Hover me
+        </button>
+        <button className="block py-2 px-4 bg-red-500 text-white font-semibold rounded-lg shadow-md hover:bg-red-700 focus:outline-none">
+          Hover me
+        </button>
+        <button className="block py-2 px-4 bg-red-500 text-white font-semibold rounded-lg shadow-md hover:bg-red-700 focus:outline-none">
+          Hover me
+        </button>
+        <button className="block py-2 px-4 bg-red-500 text-white font-semibold rounded-lg shadow-md hover:bg-red-700 focus:outline-none">
+          Hover me
+        </button>
+        <button className="block py-2 px-4 bg-red-500 text-white font-semibold rounded-lg shadow-md hover:bg-red-700 focus:outline-none">
+          Hover me
+        </button>
+        <button className="block py-2 px-4 bg-red-500 text-white font-semibold rounded-lg shadow-md hover:bg-red-700 focus:outline-none">
+          Hover me
+        </button>
+        <button className="block py-2 px-4 bg-red-500 text-white font-semibold rounded-lg shadow-md hover:bg-red-700 focus:outline-none">
+          Hover me
+        </button>
+        <button className="block py-2 px-4 bg-red-500 text-white font-semibold rounded-lg shadow-md hover:bg-red-700 focus:outline-none">
+          Hover me
+        </button>
+        <button className="block py-2 px-4 bg-red-500 text-white font-semibold rounded-lg shadow-md hover:bg-red-700 focus:outline-none">
+          Hover me
+        </button>
+        <button className="block py-2 px-4 bg-red-500 text-white font-semibold rounded-lg shadow-md hover:bg-red-700 focus:outline-none">
+          Hover me
+        </button>
+        <button className="block py-2 px-4 bg-red-500 text-white font-semibold rounded-lg shadow-md hover:bg-red-700 focus:outline-none">
+          Hover me
+        </button>
+        <button className="block py-2 px-4 bg-red-500 text-white font-semibold rounded-lg shadow-md hover:bg-red-700 focus:outline-none">
+          Hover me
+        </button>
+        <button className="block py-2 px-4 bg-red-500 text-white font-semibold rounded-lg shadow-md hover:bg-red-700 focus:outline-none">
+          Hover me
+        </button>
+        <button className="block py-2 px-4 bg-red-500 text-white font-semibold rounded-lg shadow-md hover:bg-red-700 focus:outline-none">
+          Hover me
+        </button>
+        <button className="block py-2 px-4 bg-red-500 text-white font-semibold rounded-lg shadow-md hover:bg-red-700 focus:outline-none">
+          Hover me
+        </button>
+        <button className="block py-2 px-4 bg-red-500 text-white font-semibold rounded-lg shadow-md hover:bg-red-700 focus:outline-none">
+          Hover me
+        </button>
+        <button className="block py-2 px-4 bg-red-500 text-white font-semibold rounded-lg shadow-md hover:bg-red-700 focus:outline-none">
+          Hover me
+        </button>
+        <button className="block py-2 px-4 bg-red-500 text-white font-semibold rounded-lg shadow-md hover:bg-red-700 focus:outline-none">
+          Hover me
+        </button>
+        <button className="block py-2 px-4 bg-red-500 text-white font-semibold rounded-lg shadow-md hover:bg-red-700 focus:outline-none">
+          Hover me
+        </button>
+        <button className="block py-2 px-4 bg-red-500 text-white font-semibold rounded-lg shadow-md hover:bg-red-700 focus:outline-none">
+          Hover me
+        </button>
+        <button className="block py-2 px-4 bg-red-500 text-white font-semibold rounded-lg shadow-md hover:bg-red-700 focus:outline-none">
+          Hover me
+        </button>
+        <button className="block py-2 px-4 bg-red-500 text-white font-semibold rounded-lg shadow-md hover:bg-red-700 focus:outline-none">
+          Hover me
+        </button>
+        <button className="block py-2 px-4 bg-red-500 text-white font-semibold rounded-lg shadow-md hover:bg-red-700 focus:outline-none">
+          Hover me
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
